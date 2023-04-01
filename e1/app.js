199f9b196048bc041a196b16a0979071ed922951
Nimi-2 <84320149+Nimi-2@users.noreply.github.com>
2023-04-01 19:56:28 +0200

    Update app.js

--- e1/app.js
+++ e1/app.js
@@ -4,12 +4,12 @@ const app = express();
 require('dotenv').config();
 
 const port = 8097;
-const VERSION=process.env.APP_VERSION || 'nothing';
+const VERSION=process.env.VERSION || 'nothing';
 app.get('/', (req, res) => {
   const ipAddress = req.ip.replace('::ffff:','');
   res.send(`Adres IP serwera: ${ipAddress}<br>
             Nazwa serwera: ${os.hostname()}<br>
-            Wersja aplikacji: ${process.env.APP_VERSION}`);
+            Wersja aplikacji: ${process.env.VERSION}`);
 });
 
 app.listen(port, () => {
